2b71655da62073cbdfd9ed490987adb1b19bc27a
Milos Mandic <mickozr@gmail.com>
2019-05-08 20:45:32 +0200

    email opet

--- src/components/body/Contact.js
+++ src/components/body/Contact.js
@@ -29,10 +29,13 @@ export default class Contact extends Component {
     const { name, email, message } = this.state;
     return (
       <form onSubmit={this.handleSubmit} id="contact-form" name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+        <label>
           <input type="text" name="name" value={name} onChange={this.handleChange} placeholder="your name"/>
+        </label>  
           <input type="email" name="email" value={email} onChange={this.handleChange} placeholder="your email"/>
+        <label>
           <textarea name="message" value={message} onChange={this.handleChange} rows="9" placeholder="write something"/>
-         
+        </label>   
          <input type="hidden" name="form-name" value="contact" />
         <button type="submit">Send</button>
     </form>  
