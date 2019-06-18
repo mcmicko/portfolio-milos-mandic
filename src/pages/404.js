aaffd384f1be873baecfa593ac6dce93367507e0
Milos Mandic <mickozr@gmail.com>
2019-06-18 10:20:22 +0200

    Add404

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,9 +6,11 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <div id="not-found">
-	    <h1>NOT FOUND</h1>
-	    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div id="not-found-wrapper">
+      <div id="not-found">
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </div>     
     </div>
   </Layout>
 )
