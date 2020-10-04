28a7309ce8435552823de762fc61984e1063f43c
mcmicko <mcmzrc@gmail.com>
2020-10-04 18:39:06 +0200

    fixing about circle

--- src/components/body/About.js
+++ src/components/body/About.js
@@ -17,36 +17,31 @@ export default class About extends Component {
   render() {
     return (
       <React.Fragment>
-        <div id="about">
-          <div id="about-inner">
-            <div id="about-wrapper">
-              <div id="about-content">
-                <p>
-                  Hello, my name is Milos, I'm 26 years old and I live in Zrenjanin, Serbia. <br/>
-                  I have a degree from the Technical Faculty in Zrenjanin, in the field of Industrial Engineering. <br/>
-                  {/* My passion is to programing. My hobby grew into love.  <br/> */}
-                  I am passionate about technology and how it impacts human lives. Whether we're becoming more distant with technological advancements, and generally how tech impacts our everyday lives, are types of questions I like to read on and try to find an answer somehow.
-                  I like to create something new and funcional in a blank page, and that other people benefit from it.
-                </p>
-              </div>      
-            </div>      
+        <div id="about-wrapper">
+          <div id="about-text">
+            <p>
+              Hello, my name is Milos, I'm 27 years old and I live in Zrenjanin, Serbia.
+              I have a degree from the Technical Faculty in Zrenjanin, in the field of Industrial Engineering.
+              I am passionate about technology and how it impacts human lives. Whether we're becoming more distant with technological advancements, and generally how tech impacts our everyday lives, are types of questions I like to read on and try to find an answer somehow.
+              I like to create something new and funcional in a blank page, and that other people benefit from it.
+            </p>
           </div>
-        </div>        
+        </div>
 
         <div id="lang-mobile">
-          <img className="icon-mobile" src={react} alt=""/>
-          <img className="icon-mobile" src={csharp} alt=""/>
-          <img className="icon-mobile" src={aspnett} alt=""/>
-          <img className="icon-mobile" src={nodejs} alt=""/>
-          <img className="icon-mobile" src={mongoo} alt=""/>   
-          <img className="icon-mobile" src={html} alt=""/>
-          <img className="icon-mobile" src={csss} alt=""/>
-          <img className="icon-mobile" src={jscript} alt=""/>
-          <img className="icon-mobile" src={reduxx} alt=""/>
-          <img className="icon-mobile" src={sasss} alt=""/>
-          <img className="icon-mobile" src={express} alt=""/>     
-          <img className="icon-mobile" src={gastbyy} alt=""/>     
-          <img className="icon-mobile" src={angularr} alt=""/>     
+          <img className="icon-mobile" src={react} alt="" />
+          <img className="icon-mobile" src={csharp} alt="" />
+          <img className="icon-mobile" src={aspnett} alt="" />
+          <img className="icon-mobile" src={nodejs} alt="" />
+          <img className="icon-mobile" src={mongoo} alt="" />
+          <img className="icon-mobile" src={html} alt="" />
+          <img className="icon-mobile" src={csss} alt="" />
+          <img className="icon-mobile" src={jscript} alt="" />
+          <img className="icon-mobile" src={reduxx} alt="" />
+          <img className="icon-mobile" src={sasss} alt="" />
+          <img className="icon-mobile" src={express} alt="" />
+          <img className="icon-mobile" src={gastbyy} alt="" />
+          <img className="icon-mobile" src={angularr} alt="" />
         </div>
       </React.Fragment>
 
